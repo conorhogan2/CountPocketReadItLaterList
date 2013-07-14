d02ec162a5abcc920486b994d15154433111cf7d
Conor <conor.hogan.2@gmail.com>
2013-07-14 18:51:11 +0100

    tidying up formating, noticed bad spacing while looking through git CLI

--- CountPocketList.js
+++ CountPocketList.js
@@ -1,18 +1,18 @@
 function scrollListAndReturnPos() {
-    
+
     x = 0;
     vert = document.height;
-    
+
     window.scroll(x,vert);
-    
+
     return vert;
 }
 
 function countList() {
-    
+
 var ul = document.getElementById("queue");
 var liNodes = [];
- 
+
     for (var i = 0; i < ul.childNodes.length; i++) {
      	if (ul.childNodes[i].nodeName == "LI") {
      		liNodes.push(ul.childNodes[i]);
@@ -22,23 +22,23 @@ var liNodes = [];
 }
 
 function loopScroll() {
-  
-  old = curr;
-  curr = scrollListAndReturnPos();
+
+    old = curr;
+    curr = scrollListAndReturnPos();
 
     if(curr==old) {
         clearInterval(refreshIntervalId);
-        
+
         alert(countList());
-        
+
         return;
        }
 }
 
 function main() {
-    
+
     old = 0;
     curr = scrollListAndReturnPos();
-    
+
     refreshIntervalId = setInterval(loopScroll, 1000);
 }
